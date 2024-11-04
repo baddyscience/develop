a6bd45afbe41aa764915efdcfab02e9a35336bc4
baddyscience <wunaitaisui@gmail.com>
2024-11-04 16:19:14 +0800

    comment python unit5 schoolwork

--- python/随机密码生成.py
+++ python/随机密码生成.py
@@ -2,22 +2,12 @@
 import random
 
 def genpwd(length):
-    return ''.join(str(random.randint(0,9)) for _ in range(length))
+    a = pow(10,length-1)
+    b = pow(10,length)-1
+    return random.randint(a,b)
 
 length = eval(input())
 random.seed(17)
-if length == 3:
-    print("634")
-    print("524")
-    print("926")
-elif length == 4:
-    print("9552")
-    print("7785")
-    print("5971")
-elif length == 7:
-    print("9757436")
-    print("7948585")
-    print("6090601")
-else:
-    for i in range(3):
-        print(genpwd(length))
+
+for i in range(3):
+    print(genpwd(length))
